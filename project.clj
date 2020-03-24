c1f1c9061342df6476829e8d6ec348faaa866119
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-03-24 15:39:28 -0500

    fixed issue with old core.async  reference

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject trateg "0.1.2-SNAPSHOT"
   :dependencies
-  [[org.clojure/clojure "1.10.1-RC1"]
+  [;[org.clojure/clojure "1.10.1-RC1"]
+   [org.clojure/clojure "1.10.1"]
+  ;[org.clojure/core.async "0.4.490"]
+   [org.clojure/core.async "1.0.567"]
    [medley "1.2.0"]
    [clj-time "0.14.3"] ; (bybit)
    [cheshire "5.8.1"] ; JSON encoding
@@ -8,9 +11,8 @@
    [org.clojure/data.csv "0.1.4"]
    [net.cgrand/xforms "0.18.2"] ; transducers for timeseries
    [org.ta4j/ta4j-core "0.12"] ; ta4j java technical indicator library
-   [throttler "1.0.0"] ; throtteling
-   ;[org.clojure/core.async "0.4.490"]
-   [org.clojure/core.async "0.5.527"]
+   [org.pinkgorilla/throttler "1.0.1"] ; throtteling
+   
    ]
   :repl-options {:init-ns ta.model.single}
   :source-paths ["src" "dev"]
